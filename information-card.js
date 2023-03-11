d7eec398e7c4576e0440f768046031c437c4c431
changthatsmynem <nathapab.jirasaweksil@gmail.com>
2023-03-11 15:49:43 +0700

    Task 3 complete - Information Card

---
+++ information-card.js
@@ -0,0 +1,7 @@
+let name = "Chang";
+let location = "Bangkok";
+let hobby = "reading a lof books";
+
+const aboutPerson = `Hi, my name is ${name}. I live in ${location} and I enthuse about ${hobby}.`;
+
+console.log(aboutPerson);
